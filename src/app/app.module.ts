a6551c37e6b118d4d7cb5f8143f790479c50f77f
seveshsanveo <sevesh@sanveotech.com>
2020-06-18 18:11:03 +0530

    theme added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { AppComponent } from './app.component';
 import { AngularSlickgridModule } from 'angular-slickgrid';
 import { SlickGridComponent } from './slick-grid/slick-grid.component';
 import { SearchComponent } from './search/search.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -17,7 +22,10 @@ import { SearchComponent } from './search/search.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularSlickgridModule.forRoot()
+    AngularSlickgridModule.forRoot(),
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule,MatToolbarModule,MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
