428d7a21d0a71897900fa1d71daf5f95b4504817
satishy9757 <satishy9757@gmail.com>
2020-06-20 11:52:12 +0530

    edit task partially added 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MaterialValidationComponent } from './quantities-module/material-validation/material-validation.component';
+import { PropertyMappingComponent } from './property-mapping-module/property-mapping/property-mapping.component';
+import { RoleaccessComponent } from './roleaccess/roleaccess.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [ 
+  { path: 'material', component: MaterialValidationComponent },
+  { path: 'property', component: PropertyMappingComponent },
+  { path: 'roleaccess', component: RoleaccessComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/roleaccess/roleaccess.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-roleaccess',
+  templateUrl: './roleaccess.component.html',
+  styleUrls: ['./roleaccess.component.scss']
+})
+export class RoleaccessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
